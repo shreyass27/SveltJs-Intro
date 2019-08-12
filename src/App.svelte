73c057b4df98002bffe7dbfd8942462162957567
Shreyas Shetty <shreyass2709@gmail.com>
2019-08-12 20:53:31 +0530

    Start up code

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,36 @@
 <script>
-	export let name;
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
