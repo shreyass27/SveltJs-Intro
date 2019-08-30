feb5377d21ed178465d816dec24ac2f317b76904
Shreyas Shetty <shreyass2709@gmail.com>
2019-08-30 20:48:00 +0530

    Component, Slots and Life cycle hooks

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,52 @@
 <script>
-	export let name;
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  export let name;
+  let openModal = false;
+  let closable = false;
+
+  const myName = "App";
+  const products = [
+    {
+      id: "Cooool",
+      title: "A book",
+      price: 9.99
+    },
+    {
+      id: "Coooasdads",
+      title: "A Fish",
+      price: 9.18
+    }
+  ];
+
+  function alertFunc(event) {
+    // event returns custom event dispatched
+    console.log(event);
+    alert(
+      `Just an alert | ${event.type} return : ${JSON.stringify(
+        event.detail
+      )} object`
+    );
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+
 </style>
 
-<h1>Hello {name}!</h1>
+{#each products as product (product.id)}
+  <Product {...product} on:addToCart={alertFunc} on:deleteCart={alertFunc} />
+{/each}
+
+<button on:click={() => (openModal = true)}>Show Modal</button>
+
+{#if openModal}
+  <!-- Variable prop can be passed and bind back to Parent compenent which uses SLOT -->
+  <Modal on:close={() => (openModal = false)} 
+  let:didAgree={closable}>
+    <h1 slot="header">Hello</h1>
+    <h3>This works</h3>
+    <button slot="footer" on:click={() => (openModal = false)} disabled={!closable}>Confirm</button>
+  </Modal>
+{/if}
---
+++ src/Product.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let title;
+  export let price;
+  export let id;
+  const dispatch = createEventDispatcher();
+  const myName = "Product";
+
+  function addToCart() {
+    dispatch("addToCart", {
+      id: "ItemId addToCart"
+    });
+  }
+
+  function deleteCart() {
+    dispatch("deleteCart", {
+      id: "ItemId deleteCart"
+    });
+  }
+
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <h3>&#8377;{price}</h3>
+  <button on:click={addToCart}>Add To Cart</button>
+  <button on:click={deleteCart}>Delete</button>
+</article>
